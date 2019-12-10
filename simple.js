a35dce1ba6b7fb28be587a5e9a561f4d23c79f20
Denis Larin <mr.denislarin@icloud.com>
2019-12-10 23:48:11 +0300

    second task

--- simple.js
+++ simple.js
@@ -1,6 +1,7 @@
 /*
 * Реализуйте функцию isPrime(), которая возвращает true или false, указывая, является ли переданное ей число простым.
 * */
+
 const isPrime = (number) => {
     if (number === 0 || number === 1){
         return  false;
@@ -16,3 +17,18 @@ console.log(isPrime(0));
 console.log(isPrime(1));
 console.log(isPrime(17));
 console.log(isPrime(10000000000000));
+
+/*
+* Реализуйте функцию factorial(), которая возвращает факториал переданного ей числа
+* */
+const factorial = number => {
+    let factorial = 1;
+    while (number > 1) {
+        factorial *= number;
+        number--;
+    }
+    return factorial;
+}
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(6));
