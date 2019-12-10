60bc8b5d66ad56470f6f7b849427d4e1dfc5a414
Denis Larin <mr.denislarin@icloud.com>
2019-12-10 23:59:29 +0300

    forth task

--- simple.js
+++ simple.js
@@ -47,3 +47,18 @@ console.log(fib(0)[0]);
 console.log(fib(1)[0]);
 console.log(fib(10)[0]);
 console.log(fib(20)[0]);
+
+/*
+* Реализуйте функцию isSorted(), которая возвращает true или false в зависимости о того, отсортирован ли переданный ей числовой массив.
+* */
+const isSorted = array =>{
+    for (let i = 0; i < array.length-1; i++) {
+        if (array[i] > array[i+1]){
+            return false;
+        }
+    }
+    return true;
+};
+console.log(isSorted([]));
+console.log(isSorted([-Infinity, -5, 0, 3, 9]));
+console.log(isSorted([3,9,-3,10]));
